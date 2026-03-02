6ea74ba8838d6a51ad1d12a27d0ea70051a5cd45
Rasul Ravshanbekov <frezocsss@gmail.com>
2026-03-02 23:31:34 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-let header = alert("hozir sizda random sonlar chiqadi")
-let randomSon = alert("Sizga random orqali chiqagan son: " + Math.round(Math.random() * 1000))
-alert(`${randomSon * 2}`)
-alert(`${randomSon / 2}`)
-alert(`${randomSon % 1000}`)
+let randomSon = Math.round(Math.random() * 100)
+alert("hozir sizda random sonlar chiqadi")
+alert(`Sizga random orqali chiqagan son: ${randomSon}`)
 let tastiq = confirm("random sonini 2ga bolmoqchimisiz yoki kopaytirmoqchimisiz?")
-
-
-
-alert(`Ranmdom orqali chiqgan son ${randomSon} edi \n biz uni 2ga bolganimzda ${bolish} chiqdi \n biz uni 2ga kopaytirganda ${kopaytirish} chiqdi \n biz uni 1000ga bolganda ${foyiz} chiqdi`)
+alert(`Sizga random orqali tushgan son: ${randomSon}  \n bu sonni biz 2ga bolganimzda: ${randomSon / 2}  \n bu sonnni 2ga kopaytirganimzda: ${randomSon * 2} chiqdi`)
